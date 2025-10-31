b81aec5df89f9b1334ba3a0af3be903e5191de45
Chara Kakraba-Ampeh <ck942@drexel.edu>
2025-10-31 17:33:50 -0400

    implement login endpoint

--- app/server.js
+++ app/server.js
@@ -13,9 +13,6 @@ let app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// global object for storing tokens
-// in a real app, we'd save them to a db so even if the server exits
-// users will still be logged in when it restarts
 let tokenStorage = {};
 
 pool.connect().then(() => {
@@ -71,10 +68,51 @@ app.post("/create", async(req, res) => {
         return res.sendStatus(500);
     }
 
-    // Login after creating account
+    // Auto-login after creating account
     let token = makeToken();
     tokenStorage[token] = username;
     res.cookie("token", token, cookieOptions).status(200).send("User created and logged in");
+});
+
+// Login
+app.post("/login", async (req, res) => {
+    let { username, password } = req.body;
+
+    if (!validateBody(req.body, ["username", "password"])) {
+        return res.status(400).send("Missing username or password");
+    }
+
+    let result;
+    try {
+        result = await pool.query(
+            "SELECT password_hash FROM users WHERE username = $1", 
+            [username]
+        );
+    } catch (error) {
+        console.error("SELECT failed:", error);
+        return res.sendStatus(500);
+    }
+
+    if (result.rows.length === 0) {
+        return res.status(400).send("Invalid credentials");
+    }
+
+    let hash = result.rows[0].password_hash;
+    let verifyResult;
+    try {
+        verifyResult = await argon2.verify(hash, password);
+    } catch (error) {
+        console.log("Password verificaton failed:", error);
+        return res.sendStatus(500);
+    }
+
+    if (!verifyResult) {
+        return res.status(400).send("Invalid credentials");
+    }
+
+    let token = makeToken();
+    tokenStorage[token] = username;
+    res.cookie("token", token, cookieOptions).status(200).send("Logged in successfully");
 });
 
 app.listen(port, hostname, () => {
